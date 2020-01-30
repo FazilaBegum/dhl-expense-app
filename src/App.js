eebc48230d34678512ac1e83a0b07ffbddd0e174
A <Fazila.A@unisys.com>
2020-01-30 18:10:24 +0530

    Added main component and its basic HTML layout

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import './App.css';
+import ExpenseComponent from './ExpenseComponent'
+
+class App extends React.Component {
+  constructor () {
+    super() 
+    this.state = {
+      expenseList: [],
+      openForm: false,
+
+    }
+    this.expenseType = ''
+  }
+
+  addExpense = () => {
+    let amount = document.querySelector('#amount')
+    let description = document.querySelector('#description')
+    let expenseDate = document.querySelector('#exp-date')
+    let incomeObject = {
+      order: this.state.expenseList.length + 1,
+      type: this.expenseType,
+      amount: amount.value,
+      description: description.value,
+      expenseDate: expenseDate.value,
+    }
+    this.state.expenseList.push(incomeObject)
+    this.setState({ 
+      expenseList: this.state.expenseList,
+      openForm: false
+    })
+  }
+
+  deleteExpense = (order) => {
+    this.state.expenseList.splice(order - 1, 1)
+    this.setState({expenseList: this.state.expenseList})
+  }
+
+  openForm = (expense) => {
+    this.expenseType = expense
+    this.setState({openForm: true})
+  }
+
+  render() {
+    const income = this.state.expenseList
+    .filter(expense => expense.type === 'income')
+    .map(expense => parseInt(expense.amount,10))
+    .reduce((r, a) => {
+      return r + a
+    }, 0)
+
+    const spending = this.state.expenseList
+    .filter(expense => expense.type === 'spend')
+    .map(expense => parseInt(expense.amount,10))
+    .reduce((r, a) => {
+      return r + a
+    }, 0)
+    const balance = income - spending
+
+    return (
+      <div className="App">
+        <div className="header">
+          <span className="balance-label">Balance</span>
+          <h1 className="balance">{balance} INR</h1>
+          <span className="income-label"> Income: {income}</span>
+          <span className="spending-label">Spending: {spending}</span>
+        </div>
+        {!!this.state.expenseList.length &&
+          <ExpenseComponent
+            expenseList={this.state.expenseList}
+            deleteExpense={this.deleteExpense}
+          />
+        }
+        {this.state.openForm &&
+          <div className="input-form">
+            <input id="exp-date" type="text" placeholder="Date" />
+            <input id="amount" type="text" placeholder="Amount" />
+            <input id="description" type="text" placeholder="Description" />
+            <button className="add-button" onClick={this.addExpense}> Add </button>
+          </div>
+        }
+        <button className="add-income" onClick={() => this.openForm('income')}> Add Income </button>
+        <button className="add-expense" onClick={() => this.openForm('spend')}> Add Spending </button>
+      </div>
+    );
+  }
+}
+
+export default App;
